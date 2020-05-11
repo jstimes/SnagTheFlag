59da9c4b1e8b41a4d9d6889382dece4978a5b22f
Jacob Stimes <jacob.stimes@gmail.com>
2020-05-10 21:48:01 -0700

    Add remaining levels

--- src/app/menus/free_play_menu.ts
+++ src/app/menus/free_play_menu.ts
@@ -69,7 +69,7 @@ export class FreePlayMenu implements GameModeManager {
         const buttonOffsetY = .02;
         const elementSize = new Point(.18, .08);
         const buttonTopMargin = headerTopMargin + buttonOffsetY + elementSize.y;
-        const fontSize = 24;
+        const fontSize = 22;
         const headerStyle: TextBoxStyle = {
             color: '#dddddd',
             fontSize,
