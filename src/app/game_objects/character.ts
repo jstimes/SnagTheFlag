0279a60d720686dc65f5525d79deea8e9152e217
Jacob Stimes <jacob.stimes@gmail.com>
2020-05-13 16:47:58 -0700

    Cleanup character

--- src/app/game_objects/character.ts
+++ src/app/game_objects/character.ts
@@ -14,7 +14,12 @@ const TWO_PI = Math.PI * 2;
 const CHARACTER_CIRCLE_RADIUS = Grid.TILE_SIZE / 4;
 
 interface GameDelegate {
-    getCurrentAimPath(params: { ray: Ray; startingTileCoords: Point; fromTeamIndex: number; numRicochets: number; }): Target[];
+    getCurrentAimPath(params: {
+        ray: Ray;
+        startingTileCoords: Point;
+        fromTeamIndex: number;
+        numRicochets: number;
+    }): Target[];
 }
 
 /** Represents one squad member on a team. */
@@ -36,7 +41,8 @@ export class Character {
     // Game-state.
     health: number;
     tileCoords: Point;
-    characterAbilityTypeToAbilityState: Map<CharacterAbilityType, CharacterAbilityState>;
+    characterAbilityTypeToAbilityState:
+        Map<CharacterAbilityType, CharacterAbilityState>;
     animationState: AnimationState;
     gameDelegate: GameDelegate;
 
@@ -53,7 +59,9 @@ export class Character {
             movementSpeedMs: Grid.TILE_SIZE * .005,
             isAnimating: false,
             remainingTargets: [],
-            currentCenterCanvas: Grid.getCanvasFromTileCoords(this.tileCoords).add(Grid.HALF_TILE),
+            currentCenterCanvas:
+                Grid.getCanvasFromTileCoords(this.tileCoords)
+                    .add(Grid.HALF_TILE),
         }
         this.teamIndex = params.teamIndex;
         this.index = params.index;
@@ -70,7 +78,8 @@ export class Character {
             if (extraAction.maxUses !== 0) {
                 actionState.usesLeft = extraAction.maxUses;
             }
-            this.characterAbilityTypeToAbilityState.set(extraAction.abilityType, actionState);
+            this.characterAbilityTypeToAbilityState
+                .set(extraAction.abilityType, actionState);
         }
         this.isAiming = false;
         this.aimAngleRadiansClockwise = 0;
@@ -80,7 +89,8 @@ export class Character {
     }
 
     render(context: CanvasRenderingContext2D): void {
-        const tileTopLeftCanvas = this.animationState.currentCenterCanvas.subtract(Grid.HALF_TILE);
+        const tileTopLeftCanvas =
+            this.animationState.currentCenterCanvas.subtract(Grid.HALF_TILE);
         const tileCenterCanvas = this.animationState.currentCenterCanvas;
 
         context.fillStyle = this.getCharacterColor();
@@ -95,7 +105,8 @@ export class Character {
         context.fill();
 
         // Character number.
-        const tileBottomLeftCanvas = tileTopLeftCanvas.add(new Point(0, Grid.TILE_SIZE));
+        const tileBottomLeftCanvas =
+            tileTopLeftCanvas.add(new Point(0, Grid.TILE_SIZE));
         const text = `${this.index + 1}`;
         context.fillStyle = THEME.characterTextColor;
         const fontSize = 12;
@@ -116,7 +127,9 @@ export class Character {
                 -CHARACTER_CIRCLE_RADIUS - healthBarHeight * 2));
         const remainingHealthWidth = healthBarWidth * fractionHealthLeft;
         context.fillStyle = THEME.remainingHealthBarColor;
-        context.fillRect(healthBarTopLeft.x, healthBarTopLeft.y, remainingHealthWidth, healthBarHeight);
+        context.fillRect(
+            healthBarTopLeft.x, healthBarTopLeft.y,
+            remainingHealthWidth, healthBarHeight);
         if (this.health !== this.settings.maxHealth) {
             context.fillStyle = THEME.lostHealthBarColor;
             context.fillRect(
@@ -134,7 +147,9 @@ export class Character {
                 const ankleHeight = CHARACTER_CIRCLE_RADIUS * .75;
                 const toeWidth = CHARACTER_CIRCLE_RADIUS * .75;
                 const toeHeight = ankleHeight / 2;
-                const topLeftBoot = tileCenterCanvas.add(new Point(-ankleWidth, -ankleHeight / 2));
+                const topLeftBoot =
+                    tileCenterCanvas
+                        .add(new Point(-ankleWidth, -ankleHeight / 2));
                 context.fillStyle = '#804526';
                 context.fillRect(
                     topLeftBoot.x, topLeftBoot.y,
@@ -167,8 +182,10 @@ export class Character {
                     context.closePath();
                     context.fill();
                 };
-                const topArrowStart = tileCenterCanvas.add(new Point(-arrowWidth / 2, 0));
-                const bottomArrowStart = topArrowStart.add(new Point(0, arrowHeight * 1.5));
+                const topArrowStart = tileCenterCanvas
+                    .add(new Point(-arrowWidth / 2, 0));
+                const bottomArrowStart = topArrowStart
+                    .add(new Point(0, arrowHeight * 1.5));
                 drawPathFrom(topArrowStart);
                 drawPathFrom(bottomArrowStart);
                 break;
@@ -182,18 +199,21 @@ export class Character {
                 const radius = CHARACTER_CIRCLE_RADIUS * .7;
                 const width = radius * 2;
                 const height = CHARACTER_CIRCLE_RADIUS * .2;
-                const horizontalTopLeft = tileCenterCanvas.add(new Point(-width / 2, -height / 2));
+                const horizontalTopLeft = tileCenterCanvas
+                    .add(new Point(-width / 2, -height / 2));
                 context.fillRect(
                     horizontalTopLeft.x, horizontalTopLeft.y,
                     width, height);
-                const verticalTopLeft = tileCenterCanvas.add(new Point(-height / 2, -width / 2));
+                const verticalTopLeft = tileCenterCanvas
+                    .add(new Point(-height / 2, -width / 2));
                 context.fillRect(
                     verticalTopLeft.x, verticalTopLeft.y,
                     height, width);
 
                 // Circle.
                 context.beginPath();
-                context.arc(tileCenterCanvas.x, tileCenterCanvas.y, radius, 0, TWO_PI);
+                context.arc(
+                    tileCenterCanvas.x, tileCenterCanvas.y, radius, 0, TWO_PI);
                 context.closePath();
                 context.stroke();
                 break;
@@ -221,7 +241,8 @@ export class Character {
                     context.closePath();
                     context.fill();
                 };
-                const flameStart = tileCenterCanvas.add(new Point(-flameWidth / 2, 0));
+                const flameStart = tileCenterCanvas
+                    .add(new Point(-flameWidth / 2, 0));
                 const gradient = context.createLinearGradient(
                     flameStart.x, flameStart.y - flameHeight,
                     flameStart.x + flameWidth, flameStart.y);
@@ -277,7 +298,8 @@ export class Character {
         if (this.isFinishedWithTurn || this.hasMoved) {
             throw new Error(`Already moved.`);
         }
-        this.animationState.currentCenterCanvas = Grid.getCanvasFromTileCoords(this.tileCoords).add(Grid.HALF_TILE);
+        this.animationState.currentCenterCanvas =
+            Grid.getCanvasFromTileCoords(this.tileCoords).add(Grid.HALF_TILE);
         this.animationState.currentTarget = targetsPath.shift()!;
         this.tileCoords = tileCoords;
         this.animationState.remainingTargets = targetsPath;
@@ -296,11 +318,13 @@ export class Character {
         }
         const currentTarget = this.animationState.currentTarget!;
         const direction = currentTarget.ray.direction;
-        const positionUpdate = direction.multiplyScaler(this.animationState.movementSpeedMs * elapsedMs);
+        const positionUpdate = direction
+            .multiplyScaler(this.animationState.movementSpeedMs * elapsedMs);
         const distanceUpdate = positionUpdate.getMagnitude();
 
-        this.animationState.currentCenterCanvas = this.animationState.currentCenterCanvas
-            .add(positionUpdate);
+        this.animationState.currentCenterCanvas =
+            this.animationState.currentCenterCanvas
+                .add(positionUpdate);
         const totalDistanceTravelled = currentTarget.ray.startPt
             .distanceTo(this.animationState.currentCenterCanvas);
         if (totalDistanceTravelled < currentTarget.maxDistance) {
@@ -313,13 +337,15 @@ export class Character {
             return;
         }
 
-        this.animationState.currentTarget = this.animationState.remainingTargets.shift()!;
+        this.animationState.currentTarget =
+            this.animationState.remainingTargets.shift()!;
     }
 
     skipAnimation(): void {
         const remainingTargets = this.animationState.remainingTargets;
+        const finalTargetIndex = remainingTargets.length - 1;
         const finalTarget = remainingTargets.length > 0
-            ? this.animationState.remainingTargets[this.animationState.remainingTargets.length - 1]
+            ? this.animationState.remainingTargets[finalTargetIndex]
             : this.animationState.currentTarget!;
         this.animationState.currentCenterCanvas = finalTarget.canvasCoords;
         this.animationState.remainingTargets = [];
@@ -364,9 +390,10 @@ export class Character {
         }
         this.isAiming = false;
         this.hasShot = true;
-        this.extraAbilities = this.extraAbilities.filter((ability: CharacterAbility) => {
-            return ability.isFree;
-        });
+        this.extraAbilities = this.extraAbilities
+            .filter((ability: CharacterAbility) => {
+                return ability.isFree;
+            });
         this.checkAndSetTurnOver();
 
         return this.getCurrentShotInfo();
@@ -374,11 +401,13 @@ export class Character {
 
     // TODO - shouldn't be public
     getCurrentShotInfo(): ShotInfo[] {
+        // Shoot from center of tile.
+        const tileCenter =
+            Grid.getCanvasFromTileCoords(this.tileCoords).add(Grid.HALF_TILE);
         const straightShotInfo: ShotInfo = {
             fromTeamIndex: this.teamIndex,
             fromTileCoords: this.tileCoords,
-            // Shoot from center of tile.
-            fromCanvasCoords: Grid.getCanvasFromTileCoords(this.tileCoords).add(Grid.HALF_TILE),
+            fromCanvasCoords: tileCenter,
             aimAngleRadiansClockwise: this.aimAngleRadiansClockwise,
             projectileDetails: this.settings.gun.projectileDetails,
         };
@@ -387,12 +416,13 @@ export class Character {
             const spray = this.settings.gun.spray;
             while (shotInfos.length < spray.projectiles) {
                 const offsetDirection = shotInfos.length % 2 === 0 ? 1 : -1;
-                const aimAngle = this.aimAngleRadiansClockwise + spray.offsetAngleRadians * offsetDirection;
+                const aimAngle =
+                    this.aimAngleRadiansClockwise
+                    + spray.offsetAngleRadians * offsetDirection;
                 shotInfos.push({
                     fromTeamIndex: this.teamIndex,
                     fromTileCoords: this.tileCoords,
-                    // Shoot from center of tile.
-                    fromCanvasCoords: Grid.getCanvasFromTileCoords(this.tileCoords).add(Grid.HALF_TILE),
+                    fromCanvasCoords: tileCenter,
                     aimAngleRadiansClockwise: aimAngle,
                     projectileDetails: this.settings.gun.projectileDetails,
                 });
@@ -402,10 +432,16 @@ export class Character {
     }
 
     getGrenadeAbility(): ThrowGrenadeAbility {
-        const grenadeAbility = this.extraAbilities
-            .find((ability) => ability.abilityType === CharacterAbilityType.THROW_GRENADE);
+        const grenadeAbility =
+            this.extraAbilities
+                .find((ability) => {
+                    return ability.abilityType ===
+                        CharacterAbilityType.THROW_GRENADE;
+                });
         if (grenadeAbility == null) {
-            throw new Error(`Trying to getGrenadeAction but character does not have that action`);
+            throw new Error(
+                `Trying to getGrenadeAction but character does not have ` +
+                `that action`);
         }
         return grenadeAbility as ThrowGrenadeAbility;
     }
@@ -414,23 +450,29 @@ export class Character {
         const action = this.extraAbilities
             .find((extraAbility) => extraAbility.abilityType === abilityType);
         if (action == null) {
-            throw new Error(`Character doesn't have ability for ActionType: ${abilityType}`);
+            throw new Error(
+                `Character doesn't have ability for ActionType: ` +
+                `${abilityType}`);
         }
         this.extraAbilities = this.extraAbilities
             .filter((extraAbility) => extraAbility.abilityType !== abilityType);
-        const actionState = this.characterAbilityTypeToAbilityState.get(abilityType)!;
+        const actionState =
+            this.characterAbilityTypeToAbilityState.get(abilityType)!;
         if (actionState.usesLeft) {
             actionState.usesLeft -= 1;
         }
         actionState.cooldownTurnsLeft =
             [...this.settings.extraActions]
-                .find((extraAction) => extraAction.abilityType === action.abilityType)!.cooldownTurns;
+                .find((extraAction) => {
+                    return extraAction.abilityType === action.abilityType;
+                })!.cooldownTurns;
         if (!action.isFree) {
             // Character can't shoot and use non-free actions in same turn.
             this.hasShot = true;
-            this.extraAbilities = this.extraAbilities.filter((ability: CharacterAbility) => {
-                return ability.isFree;
-            });
+            this.extraAbilities = this.extraAbilities
+                .filter((ability: CharacterAbility) => {
+                    return ability.isFree;
+                });
         }
         this.checkAndSetTurnOver();
     }
@@ -446,13 +488,24 @@ export class Character {
         const topLeftCorner =
             tileCenterCanvas.subtract(
                 new Point(CHARACTER_CIRCLE_RADIUS, CHARACTER_CIRCLE_RADIUS));
-        const topRightCorner = topLeftCorner.add(new Point(CHARACTER_CIRCLE_RADIUS * 2, 0));
-        const bottomLeftCorner = topLeftCorner.add(new Point(0, CHARACTER_CIRCLE_RADIUS * 2));
-        const bottomRightCorner = topLeftCorner.add(new Point(CHARACTER_CIRCLE_RADIUS * 2, CHARACTER_CIRCLE_RADIUS * 2));
-        const topEdge = new LineSegment(topLeftCorner, topRightCorner, new Point(0, -1));
-        const rightEdge = new LineSegment(topRightCorner, bottomRightCorner, new Point(1, 0));
-        const bottomEdge = new LineSegment(bottomLeftCorner, bottomRightCorner, new Point(0, 1));
-        const leftEdge = new LineSegment(topLeftCorner, bottomLeftCorner, new Point(-1, 0));
+        const topRightCorner =
+            topLeftCorner.add(new Point(CHARACTER_CIRCLE_RADIUS * 2, 0));
+        const bottomLeftCorner =
+            topLeftCorner.add(new Point(0, CHARACTER_CIRCLE_RADIUS * 2));
+        const bottomRightCorner =
+            topLeftCorner
+                .add(new Point(
+                    CHARACTER_CIRCLE_RADIUS * 2,
+                    CHARACTER_CIRCLE_RADIUS * 2));
+        const topEdge =
+            new LineSegment(topLeftCorner, topRightCorner, new Point(0, -1));
+        const rightEdge =
+            new LineSegment(topRightCorner, bottomRightCorner, new Point(1, 0));
+        const bottomEdge =
+            new LineSegment(
+                bottomLeftCorner, bottomRightCorner, new Point(0, 1));
+        const leftEdge =
+            new LineSegment(topLeftCorner, bottomLeftCorner, new Point(-1, 0));
         return [
             topEdge,
             rightEdge,
@@ -475,9 +528,13 @@ export class Character {
         this.hasShot = false;
         this.extraAbilities = [];
         for (const extraAbility of this.settings.extraActions) {
-            const state = this.characterAbilityTypeToAbilityState.get(extraAbility.abilityType);
+            const state =
+                this.characterAbilityTypeToAbilityState
+                    .get(extraAbility.abilityType);
             if (!state) {
-                throw new Error(`Didn't initialize characterActionsToState for ${extraAbility.abilityType}`);
+                throw new Error(
+                    `Didn't initialize characterActionsToState for ` +
+                    `${extraAbility.abilityType}`);
             }
             if (state.usesLeft !== 0 && state.cooldownTurnsLeft <= 0) {
                 this.extraAbilities.push(extraAbility);
@@ -495,7 +552,8 @@ export class Character {
             // If free actions available, need to explicitly call setTurnOver.
             return;
         }
-        if (this.hasMoved && (this.hasShot || !this.settings.gun.canFireAfterMoving)) {
+        if (this.hasMoved
+            && (this.hasShot || !this.settings.gun.canFireAfterMoving)) {
             this.setTurnOver();
             return;
         }
@@ -507,9 +565,11 @@ export class Character {
 
     private getCharacterColor(): string {
         if (this.isFinishedWithTurn) {
-            return this.teamIndex === 0 ? THEME.blueCharacterDoneColor : THEME.redCharacterDoneColor;
+            return this.teamIndex === 0 ?
+                THEME.blueCharacterDoneColor : THEME.redCharacterDoneColor;
         }
-        return this.teamIndex === 0 ? THEME.blueCharacterReadyColor : THEME.redCharacterReadyColor;
+        return this.teamIndex === 0 ?
+            THEME.blueCharacterReadyColor : THEME.redCharacterReadyColor;
     }
 
     canShoot(): boolean {
